225b57b42d66253afd496abcccfe6aeb40a72f7e
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2023-10-25 17:11:30 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -95,6 +95,9 @@ PRODUCT_PACKAGES += vndservice
 
 # Hidl Service
 PRODUCT_ENFORCE_VINTF_MANIFEST := true
+PRODUCT_PACKAGES += \
+    libhidltransport.vendor \
+    libhwbinder.vendor
 
 # Display Config
 PRODUCT_COPY_FILES += \
@@ -119,6 +122,70 @@ PRODUCT_VENDOR_PROPERTIES += \
 	persist.vendor.vibrator.hal.context.fade=true \
 	persist.vendor.vibrator.hal.context.cooldowntime=1600 \
 	persist.vendor.vibrator.hal.context.settlingtime=5000
+
+# Identity credential
+PRODUCT_PACKAGES += \
+    android.hardware.identity-support-lib.vendor:64 \
+    android.hardware.identity_credential.xml
+
+# Nos
+PRODUCT_PACKAGES += \
+    libkeymaster4support.vendor:64 \
+    libkeymint_support.vendor:64 \
+    libnos:64 \
+    libnosprotos:64 \
+    libnos_client_citadel:64 \
+    libnos_datagram:64 \
+    libnos_datagram_citadel:64 \
+    libnos_transport:64 \
+    nos_app_avb:64 \
+    nos_app_identity:64 \
+    nos_app_keymaster:64 \
+    nos_app_weaver:64 \
+    pixelpowerstats_provider_aidl_interface-cpp.vendor:64
+
+# Misc interfaces
+PRODUCT_PACKAGES += \
+    android.frameworks.stats-V1-ndk.vendor:32 \
+    android.hardware.authsecret@1.0.vendor:64 \
+    android.hardware.biometrics.common-V2-ndk.vendor:64 \
+    android.hardware.biometrics.face-V2-ndk.vendor:64 \
+    android.hardware.biometrics.face@1.0.vendor:64 \
+    android.hardware.biometrics.fingerprint-V2-ndk.vendor:64 \
+    android.hardware.input.common-V1-ndk.vendor:64 \
+    android.hardware.input.processor-V1-ndk.vendor:64 \
+    android.hardware.keymaster@3.0.vendor:64 \
+    android.hardware.keymaster@4.0.vendor:64 \
+    android.hardware.keymaster@4.1.vendor:64 \
+    android.hardware.neuralnetworks-V4-ndk.vendor:64 \
+    android.hardware.oemlock@1.0.vendor:64 \
+    android.hardware.power@1.0.vendor:64 \
+    android.hardware.power@1.1.vendor:64 \
+    android.hardware.power@1.2.vendor:64 \
+    android.hardware.radio.config@1.0.vendor \
+    android.hardware.radio.config@1.1.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.radio@1.2.vendor \
+    android.hardware.radio@1.3.vendor \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio@1.6.vendor \
+    android.hardware.secure_element@1.0.vendor:32 \
+    android.hardware.secure_element@1.1.vendor:32 \
+    android.hardware.secure_element@1.2.vendor:32 \
+    android.hardware.thermal@1.0.vendor:32 \
+    android.hardware.thermal@2.0.vendor:32 \
+    android.hardware.weaver@1.0.vendor:64 \
+    android.hardware.wifi@1.1.vendor:64 \
+    android.hardware.wifi@1.2.vendor:64 \
+    android.hardware.wifi@1.3.vendor:64 \
+    android.hardware.wifi@1.4.vendor:64 \
+    android.hardware.wifi@1.5.vendor:64 \
+    android.hardware.wifi@1.6.vendor:64 \
+    com.google.hardware.pixel.display-V4-ndk.vendor:64 \
+    com.google.hardware.pixel.display-V5-ndk.vendor \
+    com.google.hardware.pixel.display-V6-ndk.vendor
 
 # ofox
 PRODUCT_COPY_FILES += \
