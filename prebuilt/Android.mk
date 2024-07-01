858c10fb5bc5a1d09e23d2ed3ef40f8865885867
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2024-07-01 15:13:44 +0200

    lynx_prebuilt
    lynx_prebuilt

---
+++ prebuilt/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+    LOCAL_MODULE := lynx_prebuilt
+    LOCAL_MODULE_TAGS := optional
+    LOCAL_MODULE_CLASS := ETC
+    LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
+    LOCAL_HOST_REQUIRED_MODULES := depmod
+    LOCAL_POST_INSTALL_CMD += \
+        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor; \
+        cp -rf $(LOCAL_PATH)/vendor $(TARGET_RECOVERY_ROOT_OUT)/; \
+        echo "Calling depmod on lynx_prebuilt"; \
+        $(DEPMOD) -b $(TARGET_RECOVERY_ROOT_OUT)/vendor 1.1;
+include $(BUILD_PHONY_PACKAGE)
