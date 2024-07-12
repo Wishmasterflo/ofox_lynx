87a40641d824068ecafb740ee4fb3aba9215fe30
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2024-07-12 23:35:04 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,7 +115,7 @@ VENDOR_CMDLINE := "dyndbg=\"func alloc_contig_dump_pages +p\" \
                 at24.write_timeout=100 \
 		log_buf_len=1024K \
 		bootconfig \
-                exynos_drm.load_sequential=1
+                exynos_drm.load_sequential=1"
 
 # Kernel
 BOARD_KERNEL_BASE        := 0x1000000
