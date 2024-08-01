5ad00de1d3ab4713014e391378129b5b905c4b88
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2024-08-01 22:03:57 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,7 +9,6 @@ DEVICE_PATH := device/google/lynx
 
 include device/google/gs201/BoardConfig-common.mk
 
-USES_DEVICE_GOOGLE_LYNX := true
 TARGET_BOARD_INFO_FILE := device/google/lynx/board-info.txt
 TARGET_BOOTLOADER_BOARD_NAME := lynx
 
