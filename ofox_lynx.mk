b6ede43edb07206c7f124b9a101e6c2ba67261a9
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2024-08-04 22:06:56 +0200

    Update ofox_lynx.mk

--- ofox_lynx.mk
+++ ofox_lynx.mk
@@ -10,10 +10,6 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from lynx device
 $(call inherit-product, device/google/lynx/device.mk)
 
-DEVICE_USES_NO_TRUSTY := true
-USE_SWIFTSHADER := true
-BOARD_USES_SWIFTSHADER := true
-
 PRODUCT_DEVICE := lynx
 PRODUCT_NAME := ofox_lynx
 PRODUCT_BRAND := google
