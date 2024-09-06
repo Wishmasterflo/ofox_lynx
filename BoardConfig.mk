32b1e60dbca175a2869268957fb383927b27a0c0
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2024-09-06 12:41:09 +0200

    removed 2nd CPU arch
    removed 2nd CPU arch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,13 +29,6 @@ TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a55
 TARGET_CPU_VARIANT_RUNTIME := cortex-a55
 
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
-
 # Enable 64-bit for non-zygote.
 ZYGOTE_FORCE_64 := true
 
