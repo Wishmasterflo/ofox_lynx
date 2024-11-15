770301724b621e778b3623a3b893a7fe61978d80
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2024-11-15 11:17:56 +0100

    removed vanilla_build
    removed vanilla_build

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,9 +17,6 @@ export BUILD_HOSTNAME="android-build"
 export OF_USE_MAGISKBOOT=1
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 
-# Vanilla build
-export FOX_VANILLA_BUILD=1
-
 # We have a/b partitions
 export FOX_AB_DEVICE=1
 export FOX_VIRTUAL_AB_DEVICE=1
